3e5c264212f8bc3e452c96eabdd9bc2e560cf794
Logan <spam@nondefault.net>
2021-04-17 23:43:09 -0700

    acnn

--- src/index.module.js
+++ src/index.module.js
@@ -35,6 +35,7 @@ const onLoad = () => {
 
   composer = new EffectComposer(renderer);
   composer.addPass(new RenderPass(scene, camera));
+composer.addPass(new SAOPass(scene, camera, false, false));
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.target.set(0, 0, 0);
